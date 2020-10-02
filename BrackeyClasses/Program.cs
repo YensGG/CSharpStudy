ec790be35feeaca9703439fa243298569ee67b19
Jens R. Hansen <jensrustadhansen@gmail.com>
2020-10-02 16:09:09 +0200

    Cleaned up some folders. Time to dive into classes boii.

---
+++ BrackeyClasses/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BrackeyClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Wait for user input to exit.
+            Console.ReadKey();
+        }
+    }
+}
